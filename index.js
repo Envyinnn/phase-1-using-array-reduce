83c901cb6e5de791339ff26793a46010bf994e5f
Dan <daweidern@gmail.com>
2022-07-18 21:19:32 -0500

    commit tree 08967f71b5b31df5fdd8ffd944d4f60002b76abf

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const totalBatteries = batteryBatches.reduce(batterCount, 0);
+    function batterCount(finalCount, area){
+        return finalCount + area;
+    }
